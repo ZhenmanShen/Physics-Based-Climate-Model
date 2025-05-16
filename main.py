0e342d87967db28d3ed0aed40a6bde3e28f74319
Max Shen <zhs009@ucsd.edu>
2025-05-16 15:22:57 -0700

    update

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from lightning.pytorch import LightningDataModule
 from lightning.pytorch.loggers import WandbLogger
 from omegaconf import DictConfig, OmegaConf
 from torch.utils.data import DataLoader, Dataset
+import torch.nn.functional as F
 
 
 try:
@@ -328,12 +329,30 @@ class ClimateEmulationModule(pl.LightningModule):
 
     def on_fit_start(self) -> None:
         self.normalizer = self.trainer.datamodule.normalizer  # Access the normalizer from the datamodule
-
+    
+    # CHANGED
     def training_step(self, batch, batch_idx):
         x, y_true_norm = batch
         y_pred_norm = self(x)
-        loss = self.criterion(y_pred_norm, y_true_norm)
+
+        # Split predictions and targets
+        pred_tas = y_pred_norm[:, 0]
+        pred_pr = y_pred_norm[:, 1]
+        true_tas = y_true_norm[:, 0]
+        true_pr = y_true_norm[:, 1]
+
+        # Compute individual losses
+        loss_tas = F.mse_loss(pred_tas, true_tas)
+        loss_pr = F.mse_loss(pred_pr, true_pr)
+
+        # Weighted sum: prioritize tas
+        loss = 2.0 * loss_tas + 1.0 * loss_pr
+
+        # Logging
         self.log("train/loss", loss, prog_bar=True, batch_size=x.size(0))
+        self.log("train/loss_tas", loss_tas, batch_size=x.size(0))
+        self.log("train/loss_pr", loss_pr, batch_size=x.size(0))
+
         return loss
 
     def validation_step(self, batch, batch_idx):
